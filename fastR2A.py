fa4b663689ce379331db1f87d54016a8d408be2d
Jakob <xxxxx@gmail.com>
2018-01-06 15:49:43 +0800

    correct the bug of redandunt " at the start of each line

--- fastR2A.py
+++ fastR2A.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import pandas as pd
 import os
+import re
 
 class FastR2A:
     def __init__(self, fastr_dir):
@@ -9,6 +10,7 @@ class FastR2A:
     def load_data(self, f):
         data = pd.read_csv(f, sep='\t', usecols=[0, 1, 2, 4])
         data.columns = ["PLMD ID","Uniprot Accession","Position","Sequence"]
+        data["Position"] = data["Position"].map(str)
         return data
 
     def formFastr(self):
@@ -17,12 +19,17 @@ class FastR2A:
             outputFile = f.split(".")[0]
             f = self.fastr_dir + '/' + f
             data = self.load_data(f)
-            print(data.head(5))
-            # data["fasta"] = ">" + data["PLMD ID"] + "@" + data["Uniprot Accession"] + "@" + data["Position"] + "\n" \
-            #                 + data["Sequence"]
-            # data = data["fasta"]
-            # outputFile = "/data1/hbs/total_fasta/" + outputFile + ".fasta"
-            # data.to_csv(outputFile, sep="\t", index=False, mode="w", encoding="utf-8")
+            data["fasta"] = ">" + data["PLMD ID"] + "@" + data["Uniprot Accession"] + "@" + data["Position"] + "\n" \
+                            + data["Sequence"]
+            data = data["fasta"]
+            outputFile = "/data1/hbs/total_fasta/" + outputFile + ".fasta"
+            data.to_csv(outputFile, sep="\t", index=False, mode="w", encoding="utf-8")
+            content = open(outputFile,"r").read()
+            pattern = re.compile('\"', re.S)
+            content = re.sub(pattern, "", content)
+            f = open(outputFile, "w")
+            f.write(content)
+            f.close()
 
 
 if __name__ == "__main__":
